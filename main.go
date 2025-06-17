6979c6bbc389936e5fbdfe40d0e901932806ab75
htoday1 <2773851718@qq.com>
2025-06-17 20:19:00 +0800

    feat<main>:/ping接口的响应改成pong123

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+		c.JSON(200, gin.H{"message": "pong123"})
 	})
 
 	r.Run(":8083") // 默认监听 :8083
